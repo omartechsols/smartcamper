489dc5f92b71ab7fb2945b5ffc9b004f5f8d2a34
omar.barlas <omar.barlas@essilorusa.com>
2022-10-04 20:44:44 -0500

    Add project files.

---
+++ SmartCampers/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartCampers;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SmartCampers/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SmartCampers;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
